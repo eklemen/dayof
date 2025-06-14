facbc57991d4ad04e4ab163de48b65cec51815c5
eklemen <ejklemen@gmail.com>
2025-06-14 08:42:56 -0400

    Phase 1.2 firebase init

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,39 +1,19 @@
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
 
-// Initialize Firebase Admin SDK
-admin.initializeApp();
+import {onRequest} from "firebase-functions/v2/https";
+import * as logger from "firebase-functions/logger";
 
-// Regional functions for us-central1
-const regionalFunctions = functions.region('us-central1');
+// Start writing functions
+// https://firebase.google.com/docs/functions/typescript
 
-// Import invite functions
-import { 
-  sendEventInvite, 
-  validateInviteToken, 
-  acceptInvite, 
-  getInviteStats,
-  cleanupExpiredInvites,
-  resetInviteStats,
-  updateEventInviteStats,
-  handleSendGridWebhook
-} from './invites';
-
-// Export invite functions
-export const sendEventInviteFunction = sendEventInvite;
-export const validateInviteTokenFunction = validateInviteToken;
-export const acceptInviteFunction = acceptInvite;
-export const getInviteStatsFunction = getInviteStats;
-export const cleanupExpiredInvitesFunction = cleanupExpiredInvites;
-export const resetInviteStatsFunction = resetInviteStats;
-export const updateEventInviteStatsFunction = updateEventInviteStats;
-export const handleSendGridWebhookFunction = handleSendGridWebhook;
-
-// Health check function
-export const healthCheck = regionalFunctions.https.onRequest((req, res) => {
-  res.status(200).send({
-    status: 'healthy',
-    timestamp: new Date().toISOString(),
-    region: 'us-central1'
-  });
-});
+// export const helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
